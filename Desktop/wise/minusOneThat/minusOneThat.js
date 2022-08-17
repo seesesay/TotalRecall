5cf6389cdf6fb86a4e0da1405b0dc8c868fba683
Ella S <Ella Sesay>
2022-08-17 10:03:49 -0400

    first commit

---
+++ Desktop/wise/minusOneThat/minusOneThat.js
@@ -0,0 +1,13 @@
+/*Write a function called minusOnethat takes a parameter num. Assuming the argument is a number, print the argument -1.
+
+Write a function makeSentencethat takes three parameters and concatenates them into a fully formed sentence.
+
+Write a function called getLastElementthat takes a parameter arr.
+Invoke the function with an array as the argument.
+The function should print the last element within the array.*/
+
+const minusOneThat = (num) =>{
+return num-1;
+}
+console.log(minusOneThat(4));
+
